fe06a78c4fca3b994c405ae7b420553b04ca531e
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-03-17 21:39:14 -0400

    Added delete buttons
    Added delete buttons to leads. Buttons appear on hover

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
+const deleteAllBtn = document.getElementById("delete-all-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
+const deleteBtns = document.getElementsByClassName('deleteBtn')
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -23,17 +24,25 @@ function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
-            <li>
+            <li class='lead-item'>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
+                <button class='deleteBtn'>X</button>
             </li>
         `
     }
     ulEl.innerHTML = listItems
+    for(let i=0; i<deleteBtns.length; i++){
+        deleteBtns[i].addEventListener("click", () => {
+           myLeads.splice(i,1)
+           localStorage.setItem("myLeads", JSON.stringify(myLeads))
+           render(myLeads)
+        })
+    }
 }
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteAllBtn.addEventListener("click", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
